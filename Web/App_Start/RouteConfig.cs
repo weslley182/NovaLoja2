3c38ca9d2411eaadded6488bfd8384e2d59f0369
weslley182 <weslley182>
2015-03-21 23:22:33 -0300

    aula 11

--- Web/App_Start/RouteConfig.cs
+++ Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Web
@@ -11,13 +7,36 @@ namespace Web
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");       
+
+            // 1 - Home
+
+            routes.MapRoute(null, "",
+                new { controller = "Vitrine", action = "ListaProdutos", categoria = (string)null, pagina = 1 });
+
+
+            // 2 - 
+            routes.MapRoute(null,
+                "Pagina{pagina}",
+                new { controller = "Vitrine", action = "ListaProdutos", categoria = (string)null }, new { pagina = @"\d+" });
+
+
+            routes.MapRoute(null,
+                "{categoria}", new { controller = "Vitrine", action = "ListaProdutos", pagina = 1 });
+
+
+            routes.MapRoute(null,
+                "{categoria}/Pagina{pagina}", new { controller = "Vitrine", action = "ListaProdutos" }, new { pagina = @"\d+" });
+
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+             "ObterImagem",
+             "Vitrine/ObterImgem/{produtoId}",
+              new { controller = "Vitrine", action = "ObterImagem", produtoId = UrlParameter.Optional });
+
+
+            routes.MapRoute(null, "{controller}/{action}");
+
         }
     }
 }
