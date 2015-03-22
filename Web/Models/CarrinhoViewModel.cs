bd43ee6d12432feccb80d37b916df9cfad850a5f
weslley182 <weslley182>
2015-03-22 19:16:24 -0300

    carrinho 19

---
+++ Web/Models/CarrinhoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Dominio.Entidades;
+
+namespace Web.Models
+{
+    public class CarrinhoViewModel
+    {
+        public Carrinho Carrinho { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+
+}
