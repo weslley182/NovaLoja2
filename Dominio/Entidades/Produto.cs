f29aa1ac505be5992b0471b012cf33aa901cefe9
weslley182 <weslley182>
2015-03-26 00:09:12 -0300

    33

--- Dominio/Entidades/Produto.cs
+++ Dominio/Entidades/Produto.cs
@@ -8,11 +8,19 @@ namespace Dominio.Entidades
 
         [HiddenInput(DisplayValue = false)]
         public int ProdutoId { get; set; }
-        public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Digite o nome do produto")]
+        public string Nome { get; set; }
         [DataType(DataType.MultilineText)]
+
+        [Required(ErrorMessage = "Digite a descrição")]
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Digite o valor")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Valor inválido")]
         public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "Digite a categoria")]
         public string Categoria { get; set; }
     }
 }
